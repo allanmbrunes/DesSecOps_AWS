d1b8c94f2f2851a361c02da2ca782ef2757e912d
Allan Marcel Brunes <46986006+allanmbrunes@users.noreply.github.com>
2021-07-13 15:22:08 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,81 @@
+data "aws_ami" "ubuntu" { #AMI = Amazon Machine Image
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["<<ID>>"] # Canonical
+}
+
+variable "vpc_id" {
+    default = "vpc-<<ID>>"
+}
+
+variable "subnet_id" {
+    default = "subnet-<<ID>>"
+}
+
+variable "key_name" {
+    default = "keyssh"
+    #default = "DevSecOps"
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh on EC2 instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "webserver1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "webserver1"
+  }
+}
+
+
+resource "aws_instance" "webserver2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "webserver2"
+  }
+}
