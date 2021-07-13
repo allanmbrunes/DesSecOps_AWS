8eb5d0d35a3e866160f33d177f99e6b201424018
Allan Marcel Brunes <46986006+allanmbrunes@users.noreply.github.com>
2021-07-13 15:27:14 -0700

    Create install_security_agent.sh

---
+++ install_security_agent.sh
@@ -0,0 +1,6 @@
+# Run command bellow in the Session Manager - AWS to provisioning the agent. 
+
+sudo wget -q <<bash_file_link>> -P /tmp
+sudo chmod +x /tmp/install_security_agent.sh
+sudo /tmp/install_security_agent.sh
+ls -ltr /usr/bin/security_agent
